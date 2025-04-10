f6fe2a2740a17b690c2badae7f426b7e5993e080
Anupriya-pantelwar <anupriya.001@pantelwar.com>
2025-04-10 18:52:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,116 +1,48 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = ':) Correct number!';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-
-
-*/
-
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-let highscore = 0;
-
-//creating function
-const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
-  // When there is no input
-  if (!guess) {
-    //  document.querySelector('.message').textContent = ' :( No number!';
-    displayMessage(' :( No number! ');
-
-    // when player wins
-  } else if (guess === secretNumber) {
-    // document.querySelector('.message').textContent = ' :) Correct Number!';
-    displayMessage(' :) Correct Number! ');
-    document.querySelector('.number').textContent = secretNumber;
-
-    //using CSS
-    document.querySelector('body').style.backgroundColor = ' #60b347';
-
-    document.querySelector('.number').style.width = '30rem';
-
-    //setting Highscore value
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
-    }
-
-    //when guess is wrong
-  } else if (guess !== secretNumber) {
-    if (score > 1) {
-      // document.querySelector('.message').textContent =
-      // guess > secretNumber ? ' :( Too high! ' : ' :( Too Low! ';
-      displayMessage(guess > secretNumber ? ' :( Too high! ' : ' :( Too Low! ');
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      //document.querySelector('.message').textContent =
-
-      displayMessage(' * YOU LOST THE GAME...');
-      document.querySelector('.score').textContent = 0;
-    }
-  }
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
 
   /*
-    //When guess is too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = ':( Too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent =
-        ' * YOU LOST THE GAME...';
-      document.querySelector('.score').textContent = 0;
+  btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+
+  overlay.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });  */
+
+  //code is more expressive more drive ,more declarative
+
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+
+  //HOW TO RESPOND TO KEYBOARD EVENTS
+  //for keyboard events we need addEventListener
+  //keyboard events are also called global events bacause they donot have one specific event
+
+  document.addEventListener('keydown', function (e) {
+    //console.log(e.key);
+
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
     }
-
-    //When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = ' :( Too Low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent =
-        ' * YOU LOST THE GAME...';
-      document.querySelector('.score').textContent = 0;
-    }*/
-});
-
-/*
-Implement a game reset functinality, so that the player can make a new guess!
-here is how:
-
-1. select the element with the 'again' class and attach a click event handler.
-2. In the handler function, restore initial values of the score and secretNumber variables
-3. Restore the initial condition of the message, number, score and guess input field
-4. Also restore the original background color (#222) and number width (15rem)
-
-*/
-document.querySelector('.again').addEventListener('click', function () {
-  score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-
-  //document.querySelector('.message').textContent = 'Start guessing...';
-  displayMessage('Start guessing...');
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
-
-  //using CSS
-  document.querySelector('body').style.backgroundColor = '#222';
-
-  document.querySelector('.number').style.width = '15rem';
-});
+  });
+}
