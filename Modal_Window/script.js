94dd2b95c5ba812c5f7e2d36dc3c0e6aa9d84c78
Anupriya-pantelwar <anupriya.001@pantelwar.com>
2025-04-11 18:26:36 +0530

    Add files via upload

---
+++ Modal_Window/script.js
@@ -0,0 +1,48 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+  /*
+  btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+
+  overlay.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });  */
+
+  //code is more expressive more drive ,more declarative
+
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+
+  //HOW TO RESPOND TO KEYBOARD EVENTS
+  //for keyboard events we need addEventListener
+  //keyboard events are also called global events bacause they donot have one specific event
+
+  document.addEventListener('keydown', function (e) {
+    //console.log(e.key);
+
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  });
+}
